be36da23c96db9c696cc4d2d4c95f46668b3325f
Yohann Célérien <contact@yohanncelerien.com>
2017-03-23 15:46:31 +0100

    poll

---
+++ Poll.jsx
@@ -0,0 +1,56 @@
+/**
+ * Created by celeri_y on 23/03/2017.
+ */
+import React from 'react';
+import axios from 'axios';
+
+class Poll extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {proposition: ''};
+        this.state = {end: ''};
+
+        this.handleChangeProposition = this.handleChangeProposition.bind(this);
+        this.handleChangeEnd = this.handleChangeEnd.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChangeProposition(event) {
+        this.setState({proposition: event.target.value});
+    }
+
+    handleChangeEnd(event) {
+        this.setState({end: event.target.value});
+    }
+
+    handleSubmit(event) {
+        axios.post('http://navispeed.eu:7000/poll', {
+            proposition: this.state.proposition,
+            end: this.state.end
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Proposition:
+                    <input type="text" value={this.state.proposition} onChange={this.handleChangeProposition} />
+                </label>
+                <label>
+                    End:
+                    <input type="text" value={this.state.end} onChange={this.handleChangeEnd} />
+                </label>
+                <input type="button" value="Submit" onClick={this.handleSubmit}/>
+            </form>
+        );
+    }
+}
+
+export default Poll
--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Table from './App.jsx';
-import Form from './Form.jsx'
-
+import Form from './Form.jsx';
+import Poll from './Poll.jsx';
 const App = () => (
 
 <div>
     <Form />
     <Table />
+    <Poll/>
 </ div>
 );
 
